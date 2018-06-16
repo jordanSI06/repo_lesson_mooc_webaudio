476d8156d1706944014399ba68b053e0e9daed57
jordanSI06 <jordan.sintes@gmail.com>
2018-06-16 19:08:14 +0200

    Keyboard synth

---
+++ week1/js/keyboard3.js
@@ -0,0 +1,46 @@
+var audio_context = window.AudioContext || window.webkitAudioContext; //depending of device
+var con = new audio_context();
+var osc = con.createOscillator();
+
+osc.connect(con.destination);
+osc.frequency.value = 450; //frequency of the oscilator
+osc.start();
+
+var element=document.getElementById("result");
+element.innerHTML=osc.frequency.value;
+
+var element2=document.getElementById("sound");
+element2.addEventListener('keydown', function(){
+    element.innerHTML=osc.frequency.value;
+})
+
+function setNote(key){
+    console.log(key);
+    switch(key){
+        case "q":
+            osc.frequency.value=261.63;
+            break;
+        case "s":
+            osc.frequency.value=293.66;
+            break;
+        case "d":
+            osc.frequency.value=329.63;
+            break;
+        case "f":
+            osc.frequency.value=349.63;
+            break;
+        case "g":
+            osc.frequency.value=392.00;
+            break;
+        case "h":
+            osc.frequency.value=440.00;
+            break;
+        case "j":
+            osc.frequency.value=493.88;
+            break;
+        case "k":
+            osc.frequency.value=523.25;
+            break;
+    }
+}
+
