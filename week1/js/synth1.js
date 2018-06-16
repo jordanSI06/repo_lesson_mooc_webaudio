6ac89cc1585e64efbeb64a031ac38b4858fc149f
jordanSI06 <jordan.sintes@gmail.com>
2018-06-16 18:36:33 +0200

    MouseEvent and show results

--- week1/js/synth1.js
+++ week1/js/synth1.js
@@ -3,4 +3,14 @@ var con = new audio_context();
 var osc = con.createOscillator();
 
 osc.connect(con.destination);
+
+osc.frequency.value = 450; //frequency of the tune
 osc.start();
+
+var element=document.getElementById("result");
+element.innerHTML=osc.frequency.value;
+
+var element2=document.getElementById("sound");
+element2.addEventListener('mousemove', function(){
+    element.innerHTML=osc.frequency.value;
+})
